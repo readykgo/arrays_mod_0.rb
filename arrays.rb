301c433f5558e9cc9fc6c103e576f0a533044d64
Kevin Gonzalez <kevinglz619@gmail.com>
2023-02-22 14:48:13 -0800

    initial commit

---
+++ arrays.rb
@@ -0,0 +1,4 @@
+friends = ["friend 1," "friend 2", "friend 3", "friend 4"]
+numbers = [number 1, number 2, number 3,     number 4]
+ticket_prices = [10.50, 11.50, 12.50, 13.50]
+ticket_prices_paid [no, yes, no, yes]

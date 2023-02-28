8341cee5c615fde27bcba6d4f3739cb7c7816bb9
Kevin Gonzalez <kevinglz619@gmail.com>
2023-02-27 21:26:50 -0800

    add corrections to arrays.rb exercises

--- arrays.rb
+++ arrays.rb
@@ -1,13 +1,28 @@
-friends = ["friend 1," "friend 2", "friend 3", "friend 4"]
-numbers = [number 1, number 2, number 3,     number 4]
+friends = ["friend 1", "friend 2", "friend 3", "friend 4"]
+numbers = [ 1, 2, 3, 4]
 ticket_prices = [10.50, 11.50, 12.50, 13.50]
-ticket_prices_paid [no, yes, no, yes]
-# removes last element
-friends.pop
+ticket_prices_paid = [false, true, false, true]
+
+# removes last element, also returns removed element. 
+p friends.pop
+p friends
+
 # removes first element, shifting everything down by one and bringing it back (unless empty then nil)
-numbers.shift
-changes the original array, fills the front
-ticket_prices.unshift
-# push method adds on elements at the end and brings array with changes
-ticket_prices_paid.push
-# index positions are the position of an element in an array starting with 0, for example in the friends array friend 3 is actually index position 4
+p numbers.shift
+p numbers
+
+#changes the original array, fills the front with value specified (20.00)
+ticket_prices.unshift.(20.00)
+p ticket_prices
+
+# push method adds on elements at the end and brings array with changes specified (false)
+ticket_prices_paid.push(false)
+p  ticket_prices_paid
+
+# index positions are the position of an element in an array starting with 0, for example in the friends array friend 3 is actually index position 2
+p friends[2]
+
+friends = ["joe", "jeff", "alex", "justina"]
+friends.each do |friend|
+    puts "hi #{friend}!"
+end
